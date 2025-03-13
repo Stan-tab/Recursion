dd4cfd428fdadae7d42f059511bed7395e8e440e
Stan-tab <godofwarmafia2@gmail.com>
2025-03-13 16:57:55 +0500

    Merge sort solution

---
+++ mergeSort/index.js
@@ -0,0 +1,40 @@
+const unsortedArr = [3, 2, 1, 13, 8, 5, 0, 1];
+
+function mergeSort(arr) {
+	if (arr.length <= 1) return arr;
+	if (arr.length == 2 && !Array.isArray(arr[0])) {
+		return arr[0] > arr[1] ? [arr[1], arr[0]] : arr;
+	}
+	if(!Array.isArray(arr[0])) {
+		const sorted = [mergeSort(arr.slice(0, arr.length / 2)), mergeSort(arr.slice(arr.length / 2))]
+		return sorter(sorted[0], sorted[1]);
+	}
+}
+
+function sorter(arr1, arr2) {
+	let newArr = [];
+	for (let i = 0; i < arr1.length; i++) {
+		let prevLength = arr2.length;
+		let l = 0;
+		while (arr2.length !== 0 && arr1.length !== 0) {
+			if (prevLength !== arr2.length) {
+				prevLength = arr2.length;
+				l -= 1;
+			}
+			if (arr1[i] > arr2[l]) {
+				newArr.push(...arr2.splice(l, 1));
+			} else if (arr1[i] === arr2[l]) {
+				newArr = newArr.concat([...arr1.splice(i, 1), ...arr2.splice(l, 1)]);
+			} else {
+				newArr.push(...arr1.splice(i, 1));
+			}
+			++l;
+		}
+	}
+	if (arr2.length === 0) return newArr.concat(arr1);
+	if(arr1.length === 0) return newArr.concat(arr2);
+	return newArr;
+}
+
+// console.log(sorter([3, 4, 6, 8], [1, 2, 3]));
+console.log(mergeSort(unsortedArr));
